2f131c957a42e27b0cd09a09ac1714bf218de5da
hj <hj030548@163.com>
2023-07-17 17:16:56 +0800

    3

--- src/My.java
+++ src/My.java
@@ -5,5 +5,6 @@ public class My {
     public static void main(String[] args) {
         System.out.println("这是git和idead的测试");
         System.out.println("这是git和idead的测试2");
+        System.out.println("这是git和idead的测试3");
     }
 }
